071e8bf28d68600c40709db4aaa5da3fed70be10
csw <zobirofkir19@gmail.com>
2025-06-12 02:45:42 +0100

    add documentation in header component

--- resources/js/components/HeaderComponent.tsx
+++ resources/js/components/HeaderComponent.tsx
@@ -13,19 +13,45 @@ const HeaderComponent = () => {
 
   return (
     <>
-      <SocialBar />
+    {
+      /**
+       * Social Bar Component
+       */
+    }
+    <SocialBar />
 
       <hr className="border-t border-white/30 mx-auto max-w-7xl" />
 
       <header className="sticky top-0 left-0 w-full z-[9999] bg-black bg-opacity-95 backdrop-blur-sm shadow-md border-b border-white/10">
         <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
+
+          {
+            /**
+             * Logo Component
+             */
+          }
           <LogoComponent />
 
+          {
+            /**
+             * Navigation Bar Component
+             */
+          }
           <NavBar />
 
+          {
+            /**
+             * Burger Menu Button Component
+             */
+          }
           <BurgerMenuButton menuOpen={menuOpen} toggleMenu={toggleMenu} />
         </div>
-
+        
+        {
+          /**
+           * Sidebar Component
+           */
+        }
         <AnimatePresence>
           {menuOpen && <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />}
         </AnimatePresence>
