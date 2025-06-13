ea886f66d4df9da246a5529e86e2058a853a0434
csw <zobirofkir19@gmail.com>
2025-06-13 10:37:34 +0100

    create video component

--- resources/js/components/VideoComponent.tsx
+++ resources/js/components/VideoComponent.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
 const VideoComponent = () => {
   return (
-    <div>VideoComponent</div>
-  )
-}
+    <section className="bg-white text-black py-24">
+      <div className="max-w-full mx-auto text-center px-4">
+        <h2 className="text-4xl font-black uppercase mb-12 tracking-widest">
+          Découvrez Notre Artisanat
+        </h2>
 
-export default VideoComponent
+        <div className="relative overflow-hidden rounded-3xl shadow-lg border border-black mx-auto max-w-full aspect-video">
+          <iframe
+            className="w-full h-full rounded-3xl"
+            src="https://www.youtube.com/embed/kYOP52BUZTI"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default VideoComponent;
