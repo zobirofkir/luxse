bdf161f4f099d9ee7b31eb30e7b8f8b6b905343a
csw <zobirofkir19@gmail.com>
2025-06-13 17:59:48 +0100

    update

--- resources/js/components/VideoComponent.tsx
+++ resources/js/components/VideoComponent.tsx
@@ -4,9 +4,9 @@ import { motion } from "framer-motion";
 const VideoComponent = () => {
   return (
     <section className="bg-white text-black py-24 px-4 sm:px-10">
-      <div className="container mx-auto">
+      <div className="mx-auto">
         {/* Titre et sous-titre avec flesh */}
-        <div className="flex flex-col sm:flex-row justify-between items-center mb-16">
+        {/* <div className="flex flex-col sm:flex-row justify-between items-center mb-16 container">
           <div className="flex items-center gap-4 mb-4 sm:mb-0">
             <div className="w-1 h-12 bg-rose-500 rounded-full"></div>
             <motion.h2
@@ -17,7 +17,7 @@ const VideoComponent = () => {
             >
               Découvrez Notre Artisanat
             </motion.h2>
-          </div>
+          </div> */}
 
           <motion.p
             initial={{ opacity: 0, x: 30 }}
