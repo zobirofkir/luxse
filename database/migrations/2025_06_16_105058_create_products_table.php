9cca684154edb405e71f6020a1f13bc0ff13f787
csw <zobirofkir19@gmail.com>
2025-06-16 11:54:03 +0100

    create logical columsn

--- database/migrations/2025_06_16_105058_create_products_table.php
+++ database/migrations/2025_06_16_105058_create_products_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId("category_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('name'); 
+            $table->string('slug')->unique(); 
+            $table->text('description')->nullable(); 
+            $table->decimal('price', 10, 2); 
+            $table->integer('stock')->default(0); 
+            $table->string('material')->nullable(); 
+            $table->string('size')->nullable(); 
+            $table->string('image')->nullable(); 
+            $table->enum('status', ['active', 'inactive'])->default('active'); 
             $table->timestamps();
         });
     }
