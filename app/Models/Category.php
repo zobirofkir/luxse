baf959375733f29511fd90d5d69a1c5491f755c9
csw <zobirofkir19@gmail.com>
2025-06-16 12:00:23 +0100

    add relation sheep between products and categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -12,4 +13,12 @@ class Category extends Model
         "slug",
         "description"
     ];
+
+    /**
+     * Add Relation Sheep
+     */
+    public function products() : HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -25,7 +26,7 @@ class Product extends Model
     /**
      * Relationship with categories (Category)
      */
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
