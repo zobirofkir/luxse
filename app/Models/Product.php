bd2f913d6705b537db71a4d44c6b18f54c6d6e8b
csw <zobirofkir19@gmail.com>
2025-06-16 11:59:15 +0100

    create

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'material',
+        'size',
+        'image',
+        'status',
+    ];
+
+    /**
+     * Relationship with categories (Category)
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
