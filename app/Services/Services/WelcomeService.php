2829d614c6216530008139ed7804cd2576a629ff
csw <zobirofkir19@gmail.com>
2025-06-19 21:47:05 +0100

    create product

--- app/Services/Services/WelcomeService.php
+++ app/Services/Services/WelcomeService.php
@@ -2,9 +2,20 @@
 
 namespace App\Services\Services;
 
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
+use App\Models\Product;
 use App\Services\Constructors\WelcomeConstructor;
 
 class WelcomeService implements WelcomeConstructor
 {
-    //
+    public function index() 
+    {
+        return inertia('WelcomePage', [
+            'categories' => CategoryResource::collection(Category::all())->resolve(),
+            "products" => ProductResource::collection(Product::all())->resolve()
+        ]);
+    }
+
 }
