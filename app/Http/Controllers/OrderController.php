90c3752684894f92118cdb0a7845fb0f669620d4
csw <zobirofkir19@gmail.com>
2025-06-19 21:21:45 +0100

    create order facade

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,10 +6,35 @@ use App\Http\Requests\OrderRequest;
 use App\Mail\OrderPlacedMail;
 use App\Models\Order;
 use App\Models\Product;
+use App\Services\Facades\OrderFacade;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 
 class OrderController extends Controller
 {
+    /**
+     * List Orders
+     */
+    public function index()
+    {
+        return OrderFacade::index();
+    }
+
+    /**
+     * Store Order
+     */
+    public function store(OrderRequest $request)
+    {
+        return OrderFacade::store($request);
+    }
+
+    /**
+     * Show Specific Order
+     */
+    public function show($id)
+    {
+        return OrderFacade::show($id);
+    }
+
 }
