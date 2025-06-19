853bb39a690618d50e1f413d2fe183f0d3d1b4e8
csw <zobirofkir19@gmail.com>
2025-06-19 21:31:40 +0100

    create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,9 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use App\Services\Facades\ProductFacade;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    /**
+     * Product page
+     */
+    public function index()
+    {
+        return ProductFacade::index();
+    }
+
+    /**
+     * Product detail page
+     *
+     * @param $slug
+     */
+    public function show($slug)
+    {
+        return ProductFacade::show($slug);
+    }
+
+    /**
+     * Filter Products By Category
+     */
+    public function categoryProductBySlug($slug)
+    {
+        return ProductFacade::categoryProductBySlug($slug);
+    }
+
 }
