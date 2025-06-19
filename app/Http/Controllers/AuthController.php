93585656ba6bd0fe36de8d732c1e459009e06d74
csw <zobirofkir19@gmail.com>
2025-06-19 18:51:32 +0100

    create auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,10 +5,49 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Services\Facades\AuthFacade;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    /**
+     * Register Form
+     */
+    public function registerForm()
+    {
+        return AuthFacade::registerForm();
+    }
 
+    /**
+     * Register New User
+     */
+    public function register(RegisterRequest $request)
+    {
+        return AuthFacade::register();
+    }
+
+    /**
+     * Login Form
+     */
+    public function loginForm()
+    {
+        return AuthFacade::loginForm();
+    }
+
+    /**
+     * Login User
+     */
+    public function login(LoginRequest $request)
+    {
+        return AuthFacade::login();
+    }
+
+    /**
+     * Logout Auth User
+     */
+    public function logout()
+    {
+        return AuthFacade::logout();
+    }
 }
