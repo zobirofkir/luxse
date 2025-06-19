2ce5a5f13df536e74733f20e925643c12ba6a430
csw <zobirofkir19@gmail.com>
2025-06-19 18:48:25 +0100

    create auth constructor

--- app/Services/Constructors/AuthConstructor.php
+++ app/Services/Constructors/AuthConstructor.php
@@ -2,7 +2,33 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
 interface AuthConstructor
 {
-    //
+    /**
+     * Register Form
+     */
+    public function registerForm();
+
+    /**
+     * Register New User
+     */
+    public function register(RegisterRequest $request);
+
+    /**
+     * Login Form
+     */
+    public function loginForm(LoginRequest $request);
+
+    /**
+     * Login User
+     */
+    public function login();
+
+    /**
+     * Logout Auth User
+     */
+    public function logout();
 }
