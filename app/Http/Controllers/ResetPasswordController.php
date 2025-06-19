ca949ee8dcc44cfc2bbdf254a48eae480dcf2323
csw <zobirofkir19@gmail.com>
2025-06-19 21:43:08 +0100

    create reset password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ResetPasswordRequest;
+use App\Services\Facades\ResetPasswordFacade;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
+    /**
+     * Reset Password Page
+     */
+    public function resetPassword()
+    {
+        return ResetPasswordFacade::resetPassword();
+    }
+
+    /**
+     * Send Reset Email Link
+     */
+    public function sendResetLinkEmail(ResetPasswordRequest $request)
+    {
+        return ResetPasswordFacade::sendResetLinkEmail($request);
+    }
+
 }
