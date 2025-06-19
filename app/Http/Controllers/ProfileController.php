b2d87ea2cb34fadda5fe404cf12cc7c30af04599
csw <zobirofkir19@gmail.com>
2025-06-19 21:36:03 +0100

    create profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CurrentAuthUserResource;
+use App\Services\Facades\ProductFacade;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -10,4 +11,20 @@ use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
+    /**
+     * Affiche le profil de l'utilisateur connecté
+     */
+    public function index(Request $request)
+    {
+        return ProductFacade::index($request);
+    }
+
+    /**
+     * Met à jour le profil
+     */
+    public function update(Request $request)
+    {
+        return ProductFacade::update($request);
+    }
+
 }
