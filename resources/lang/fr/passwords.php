9d7f2f462be69b00454bc55b664fc35ff488c451
csw <zobirofkir19@gmail.com>
2025-06-19 13:06:53 +0100

    update

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé par e-mail le lien de réinitialisation de votre mot de passe !',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation du mot de passe est invalide.',
+    'user' => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
+];
