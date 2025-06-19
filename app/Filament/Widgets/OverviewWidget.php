d1ef564212a949aea1786c9a786fb8af845ab734
csw <zobirofkir19@gmail.com>
2025-06-19 22:10:43 +0100

    add over view users

--- app/Filament/Widgets/OverviewWidget.php
+++ app/Filament/Widgets/OverviewWidget.php
@@ -9,23 +9,34 @@ use App\Models\Project;
 use App\Models\Service;
 use App\Models\Tag;
 use App\Models\User;
+use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class AperçuWidget extends BaseWidget
+class OverviewWidget extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            Stat::make('Produits publiés', Product::count())
-                ->description('Nombre total de produits publiés')
-                ->color('warning')
-                ->icon('heroicon-o-document-text'),
+            Stat::make('Produits en vitrine', Product::count())
+                ->description('Nombre total de bijoux publiés')
+                ->color('success')
+                ->icon('heroicon-o-sparkles'),
 
-            Stat::make('Catégories créées', Category::count())
-                ->description('Nombre total de catégories créées')
-                ->color('warning')
-                ->icon('heroicon-o-rectangle-group'),
+            Stat::make('Commandes reçues', Order::count())
+                ->description('Nombre total de commandes effectuées')
+                ->color('primary')
+                ->icon('heroicon-o-credit-card'),
+
+            Stat::make('Catégories de bijoux', Category::count())
+                ->description('Nombre de catégories disponibles')
+                ->color('info')
+                ->icon('heroicon-o-clipboard-document'),
+
+            Stat::make('Utilisateurs inscrits', User::count())
+                ->description('Nombre total de clients enregistrés')
+                ->color('secondary')
+                ->icon('heroicon-o-user-group'),
         ];
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\OverviewWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -37,7 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                //
+                OverviewWidget::class
             ])
             ->middleware([
                 EncryptCookies::class,
