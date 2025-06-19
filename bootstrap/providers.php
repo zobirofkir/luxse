83e15e18e14e522237081100755a44c3ad0c2165
csw <zobirofkir19@gmail.com>
2025-06-19 18:57:12 +0100

    fix error from category controller

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
+    App\Providers\CategoryServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
 ];
