746eb4e8a209bd7287a9193196daf0e55beae6ed
csw <zobirofkir19@gmail.com>
2025-06-19 21:28:44 +0100

    create product constructor

--- app/Services/Constructors/ProductConstructor.php
+++ app/Services/Constructors/ProductConstructor.php
@@ -4,5 +4,20 @@ namespace App\Services\Constructors;
 
 interface ProductConstructor
 {
-    //
+    /**
+     * Product page
+     */
+    public function index();
+
+    /**
+     * Product detail page
+     *
+     * @param $slug
+     */
+    public function show($slug);
+
+    /**
+     * Filter Products By Category
+     */
+    public function categoryProductBySlug($slug);
 }
