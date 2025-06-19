3d3546a4f2bcccb9a4ac46efc452fd42f7da36d9
csw <zobirofkir19@gmail.com>
2025-06-19 21:18:47 +0100

    create order constructor

--- app/Services/Constructors/OrderConstructor.php
+++ app/Services/Constructors/OrderConstructor.php
@@ -2,7 +2,22 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Requests\OrderRequest;
+
 interface OrderConstructor
 {
-    //
+    /**
+     * List Orders
+     */
+    public function index();
+
+    /**
+     * Store Order
+     */
+    public function store(OrderRequest $request);
+
+    /**
+     * Show Specific Order
+     */
+    public function show($id);
 }
