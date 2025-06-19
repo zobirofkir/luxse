bbb514f9f76f491c3e75fb180b532246940f8dbc
csw <zobirofkir19@gmail.com>
2025-06-19 14:35:24 +0100

    change

--- resources/js/pages/auth/user/ProfilePage.tsx
+++ resources/js/pages/auth/user/ProfilePage.tsx
@@ -68,7 +68,7 @@ const ProfilePage = ({ auth }) => {
 
   return (
     <AuthLayout>
-      <Head title="Profil" />
+      <Head title="Profile" />
 
       <section className="max-w-4xl mx-auto px-4 py-10">
         {/* En-tête */}
