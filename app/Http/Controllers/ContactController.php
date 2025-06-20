71f55d783f88af6f7a29e4185cbc3f5becee7af2
csw <zobirofkir19@gmail.com>
2025-06-20 10:23:21 +0100

    create contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     /**
-     * Contact Page
+     * Afficher la page de contact
      */
     public function index()
     {
-        return inertia("contacts/ContactPage");
+        return inertia('contacts/ContactPage');
+    }
+
+    /**
+     * Enregistrer un message de contact
+     */
+    public function store(ContactRequest $request)
+    {
+        Contact::create($request->validated());
+        return redirect()->back()->with('success', 'Message envoyé avec succès.');
     }
 }
