34e234310e733d7c83b99165b6825bc8dcf78474
csw <zobirofkir19@gmail.com>
2025-06-20 10:31:54 +0100

    create contact mail

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this->subject('New Contact Message')
+                    ->to('zobirofkir19@gmail.com')
+                    ->view('emails.contact-message');
+    }
+}
