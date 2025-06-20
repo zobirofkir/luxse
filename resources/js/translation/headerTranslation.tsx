2ecd026172d27501f799b791662b21bbaaf7b503
csw <zobirofkir19@gmail.com>
2025-06-20 10:44:53 +0100

    create header translation

---
+++ resources/js/translation/headerTranslation.tsx
@@ -0,0 +1,32 @@
+export const headerTranslation = {
+  fr: {
+    home: 'Accueil',
+    about: 'À propos',
+    categories: 'Catégories',
+    products: 'Produits',
+    faq: 'FAQ',
+    contact: 'Contact',
+    login: 'Connexion',
+    register: 'Inscription',
+  },
+  ar: {
+    home: 'الرئيسية',
+    about: 'حول',
+    categories: 'التصنيفات',
+    products: 'المنتجات',
+    faq: 'الأسئلة الشائعة',
+    contact: 'اتصل بنا',
+    login: 'تسجيل الدخول',
+    register: 'إنشاء حساب',
+  },
+  en: {
+    home: 'Home',
+    about: 'About',
+    categories: 'Categories',
+    products: 'Products',
+    faq: 'FAQ',
+    contact: 'Contact',
+    login: 'Login',
+    register: 'Register',
+  },
+}
